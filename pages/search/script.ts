669171f3260bd17db68922238194b332867b38d0
GradusFrontend <ruslandjavadov998@gmail.com>
2024-04-12 23:32:05 +0500

    cart page done and user page little bit

---
+++ pages/search/script.ts
@@ -0,0 +1,51 @@
+import axios from "axios"
+
+const openCatalogBtn = document.querySelector('.catalog_btn') as HTMLButtonElement
+const catalog = document.querySelector('.catalog_body') as HTMLDivElement
+const backdrop = document.querySelector('.backdrop') as HTMLDivElement
+
+openCatalogBtn.onclick = () => {
+    if (!openCatalogBtn.classList.contains('active_catalog')) {
+        openCatalogBtn.classList.add('active_catalog')
+        backdrop.classList.remove('hiden')
+        catalog.classList.remove('hiden')
+    } else {
+        openCatalogBtn.classList.remove('active_catalog')
+        backdrop.classList.add('hiden')
+        catalog.classList.add('hiden')
+    }
+}
+
+const body = document.body as HTMLBodyElement
+const appSearchInp = document.querySelector('#app_query') as HTMLInputElement
+const app_search_active_wrap = document.querySelector('.search_active_wrap') as HTMLDivElement
+const return_btn = document.querySelector('.return') as HTMLButtonElement
+
+
+appSearchInp.onfocus = () => {
+    return_btn.classList.remove('hiden')
+    app_search_active_wrap.classList.remove('hiden')
+    body.style.height = '100vh'
+    body.style.overflowY = 'hidden'
+}
+
+return_btn.onclick = () => {
+    return_btn.classList.add('hiden')
+    app_search_active_wrap.classList.add('hiden')
+    body.style.height = '100%'
+    body.style.overflowY = 'visible'
+}
+
+// axios.get('http://localhost:8080/goods')
+//     .then(res => {
+//         console.log(res.data);
+
+//         console.log(res.data.filter(item => {
+//             if (item.title.toLowerCase().includes('sum'))
+//                 console.log(item);
+//         }))
+//     })
+
+    // let arr =[1,3,4,'4333']
+    // console.log(arr.includes('333'))
+
